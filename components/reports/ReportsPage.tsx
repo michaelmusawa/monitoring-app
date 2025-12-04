7c734865b5e7f97fe0d416512fb20157c840b400
musawa <musawa@kali.kali>
2025-12-04 08:11:42 +0300

    trackeddllr

--- components/reports/ReportsPage.tsx
+++ components/reports/ReportsPage.tsx
@@ -43,7 +43,7 @@ export default function ReportsClient({
   trackerProgress,
   timeline,
   attachments,
-}) {
+}: any) {
   // =============================================================
   // CHART CONFIGS
   // =============================================================
